615c9e065e73ef0edea0d8dd6c9e5d18322dd707
shengboxu <shengboxu@indeed.com>
2018-09-03 08:53:03 +0900

    add: template

---
+++ src/life/sideworks/template/StringDisplay.java
@@ -0,0 +1,38 @@
+package life.sideworks.template;
+
+/**
+ * @author shengboxu
+ */
+
+public class StringDisplay extends AbstractDisplay {
+    private String string;
+    private int width;
+
+    public StringDisplay(String string) {
+        this.string = string;
+        this.width = string.getBytes().length;
+    }
+
+    @Override
+    public void open() {
+        this.printLine();
+    }
+
+    @Override
+    public void print() {
+        System.out.println("|" + this.string + "|");
+    }
+
+    @Override
+    public void close() {
+        this.printLine();
+    }
+
+    private void printLine() {
+        System.out.print("*");
+        for (int i = 0; i < this.width; i++) {
+            System.out.print("-");
+        }
+        System.out.println("+");
+    }
+}
