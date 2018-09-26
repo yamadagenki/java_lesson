834c608700410041c8ef62e787c66ae0e54c7764
shengboxu <shengboxu@indeed.com>
2018-09-26 09:22:33 +0900

    add: Singleton

---
+++ src/life/sideworks/SingletonSample.java
@@ -0,0 +1,21 @@
+package life.sideworks;
+
+import life.sideworks.singleton.Singleton;
+
+/**
+ * @author shengboxu
+ */
+
+public class SingletonSample {
+    public static void main(String[] args) {
+        System.out.println("Start.");
+        Singleton obj1 = Singleton.getInstance();
+        Singleton obj2 = Singleton.getInstance();
+        if (obj1 == obj2) {
+            System.out.println("obj1 and obj2 are the same instance");
+        } else {
+            System.out.println("not the same instance");
+        }
+        System.out.println("End");
+    }
+}
